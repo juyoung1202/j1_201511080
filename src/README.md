f2f47d766073974c9db41e54b3514375089eb1af
MacBook <macbook@MacBookui-MacBook-Pro.local>
2016-04-04 23:51:10 +0900

    weq

--- src/README.md
+++ src/README.md
@@ -1,1 +1,3 @@
+안녕하세요 강주영입니:
+
 Programing homework
--- src/com/j2/wk6/FileDemoMain.java
+++ src/com/j2/wk6/FileDemoMain.java
@@ -7,7 +7,6 @@
 
 package com.j2.wk6;
 
-import java.io.File.*;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -15,37 +14,43 @@ import java.io.BufferedReader;;
 import java.io.Reader;
 import java.io.File;
 
-public class FileDemoMain {
-  public static void main(String args[]) throws Exception{ //예외발생처리
-   int i;
-   char c;
-   InputStream isReadme = null;
-        InputStreamReader isrReadme = null;
-        BufferedReader brReadme=null;
-   try{
-            File myhome=new File(System.getProperty("user.home"));
-            System.out.println("myhome is "+myhome);
 
-            String cwd=new File( "." ).getCanonicalPath();
-            System.out.println("current working dir is "+cwd);
+public class FileDemoMain{
+  public static void main(String[] args) throws Exception{
+    
+    InputStream isReadme = null;
+    InputStream iskortxt = null;
+    BufferedReader brkortxt = null;
+    
+    try{
+      char c;
+      int i;
+      File myhome = new File(System.getProperty("user.home"));//홈디렉토리
+      System.out.println("myhome is "+myhome);
 
-            File readme=new File(cwd,"README.md");
-            System.out.println("readme file is "+readme);
+      String cwd=new File( "." ).getCanonicalPath();
+      System.out.println("current working dir is "+cwd);
 
-            isReadme=new FileInputStream(readme);
+      File readme=new File(cwd,"abc.txt");
+      System.out.println("readme file is "+readme);
+
+      isReadme=new FileInputStream(readme);
             System.out.println("Characters printed:");
-            while((i=isReadme.read())!=-1) {
-                c=(char)i;
-                System.out.print(c);
-            }
-        }catch(Exception e){
-            e.printStackTrace();
-        }finally{
-            if(isReadme != null)
-                isReadme.close();
-        }
+      while((i=isReadme.read()) != -1){
+        c = (char)i;
+        System.out.print(c);
+      }
+    }
+    catch(Exception e){
+      e.printStackTrace();
+    }
+    finally{
+      if(isReadme !=null)
+        isReadme.close();
+    }
+    
+    
   }
 }
       
-                                      
-      
+                      
