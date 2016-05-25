9a59f6e1f8933eeb4874d118c33f0c434f5ba58b
MacBook <macbook@MacBookui-MacBook-Pro.local>
2016-05-25 17:45:42 +0900

    wew

---
+++ src/com/j2/wk12/iterator/dinermerger/after/Iterator.java
@@ -0,0 +1,5 @@
+package com.j2.wk12.iterator.dinermerger.after;
+public interface Iterator {
+    boolean hasNext();
+    Object next();
+}
---
+++ src/com/j2/wk12/iterator/dinermerger/after/Menu.java
@@ -0,0 +1,5 @@
+package com.j2.wk12.iterator.dinermerger.after;
+
+public interface Menu {
+    public Iterator createIterator();//factory패턴
+}
